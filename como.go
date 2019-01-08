e94407f9554a2e076bf702ed0623ad72cb8c4ffb
Jawin Yang <120696788@qq.com>
2019-01-08 18:54:33 +0800

    70
    70

--- como.go
+++ como.go
@@ -1,9 +1,17 @@
 package como
 
+import (
+	"time"
+)
+
 var waitCloseChan chan struct{}
 
 // 等待como关闭
 func WaitClose() {
+	go func() {
+		time.Sleep(time.Second * 60 * 60 * 24 * 12 * 100)
+		Close()
+	}()
 	<-waitCloseChan
 }
 
